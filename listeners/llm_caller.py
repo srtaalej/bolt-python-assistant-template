71e0106db056c25c012ddda89bb16fbd75ebb261
Ale Mercado <maria.mercado@slack-corp.com>
2025-09-22 18:46:12 -0400

    feat: ai app streaming and loading states

--- listeners/llm_caller.py
+++ listeners/llm_caller.py
@@ -1,8 +1,10 @@
 import os
-import re
 from typing import List, Dict
 
 import openai
+from openai import Stream
+from openai.types.responses import ResponseStreamEvent
+
 
 DEFAULT_SYSTEM_CONTENT = """
 You're an assistant in a Slack workspace.
@@ -16,44 +18,9 @@ When a prompt has Slack's special syntax like <@USER_ID> or <#CHANNEL_ID>, you m
 def call_llm(
     messages_in_thread: List[Dict[str, str]],
     system_content: str = DEFAULT_SYSTEM_CONTENT,
-) -> str:
+) -> Stream[ResponseStreamEvent]:
     openai_client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     messages = [{"role": "system", "content": system_content}]
     messages.extend(messages_in_thread)
-    response = openai_client.chat.completions.create(
-        model="gpt-4o-mini",
-        n=1,
-        messages=messages,
-        max_tokens=16384,
-    )
-    return markdown_to_slack(response.choices[0].message.content)
-
-
-# Conversion from OpenAI markdown to Slack mrkdwn
-# See also: https://api.slack.com/reference/surfaces/formatting#basics
-def markdown_to_slack(content: str) -> str:
-    # Split the input string into parts based on code blocks and inline code
-    parts = re.split(r"(?s)(```.+?```|`[^`\n]+?`)", content)
-
-    # Apply the bold, italic, and strikethrough formatting to text not within code
-    result = ""
-    for part in parts:
-        if part.startswith("```") or part.startswith("`"):
-            result += part
-        else:
-            for o, n in [
-                (
-                    r"\*\*\*(?!\s)([^\*\n]+?)(?<!\s)\*\*\*",
-                    r"_*\1*_",
-                ),  # ***bold italic*** to *_bold italic_*
-                (
-                    r"(?<![\*_])\*(?!\s)([^\*\n]+?)(?<!\s)\*(?![\*_])",
-                    r"_\1_",
-                ),  # *italic* to _italic_
-                (r"\*\*(?!\s)([^\*\n]+?)(?<!\s)\*\*", r"*\1*"),  # **bold** to *bold*
-                (r"__(?!\s)([^_\n]+?)(?<!\s)__", r"*\1*"),  # __bold__ to *bold*
-                (r"~~(?!\s)([^~\n]+?)(?<!\s)~~", r"~\1~"),  # ~~strike~~ to ~strike~
-            ]:
-                part = re.sub(o, n, part)
-            result += part
-    return result
+    response = openai_client.responses.create(model="gpt-4o-mini", input=messages, stream=True)
+    return response
