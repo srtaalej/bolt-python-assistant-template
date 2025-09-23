bfd4649c9defb9daf5a2fbbf65cd98399a694a56
Ale Mercado <maria.mercado@slack-corp.com>
2025-09-23 17:34:55 -0400

    fix: address pr feedback

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ import logging
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 
 from listeners import register_listeners
 
 # Initialization
 logging.basicConfig(level=logging.DEBUG)
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    client=WebClient(
+        base_url=os.environ.get("SLACK_API_URL", "https://slack.com/api"),
+        token=os.environ.get("SLACK_BOT_TOKEN"),
+    ),
+)
 # Register Listeners
 register_listeners(app)
 
