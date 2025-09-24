66b5404ae167795dcb2b6938fd31bd9c6eb6e912
Ale Mercado <maria.mercado@slack-corp.com>
2025-09-24 13:46:17 -0400

    feat: feedback block

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -1,9 +1,11 @@
 from listeners.assistant import assistant
+from listeners.assistant.assistant import handle_feedback
 
 
 def register_listeners(app):
     # Using assistant middleware is the recommended way.
     app.assistant(assistant)
+    app.action("feedback")(handle_feedback)
 
     # The following event listeners demonstrate how to implement the same on your own.
     # from listeners import events
